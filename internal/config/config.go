cf1ecf5d066eea724cb61e10a0636d064cd4b0b2
tx3stn <14163530+tx3stn@users.noreply.github.com>
2022-06-08 14:50:40 +0100

    Update git config and squash to remove references to old username.
    Initial functionality (#1)
    
    Add golangci-lint config and github workflows (#2)
    
    Add test coverage summary report (#3)
    
    Create dependabot.yml
    
    Bump github.com/spf13/cobra from 1.4.0 to 1.5.0 (#4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: tx3stn <14163530+tx3stn@users.noreply.github.com>
    
    Expand variables in templates (#5)
    
    Customise file name generation (#6)
    
    Support prompts for dir name (#7)
    
    Add goreleaser config to release binary on merge (#9)
    
    set creds in workflow (#10)
    
    set tag message in command to skip editor (#11)
    
    Bump github.com/spf13/cobra from 1.5.0 to 1.6.0 (#8)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    Add copy command (#12)
    
    Add JSON schema (#13)
    
    Use viper for config (#14)
    
    Sort pickers and fix json schema (#15)
    
    Quality of life improvements and fleshing out README (#17)
    
    More variables in file names and README demos (#18)
    
    Create LICENSE (#19)
    
    Update dependabot config (#23)
    
    Remove anchors in dependabot config (#24)
    
    Update deps (#25)
    
    update inline with username update
    
    update missed gorelaser config

---
+++ internal/config/config.go
@@ -0,0 +1,54 @@
+// Package config contains logic related to user config files.
+package config
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/spf13/viper"
+	"github.com/tx3stn/pkb/internal/sentinel"
+)
+
+// CtxKey is the type for the config that gets bound to the cobra context
+// so config values can be accessed by cobra commands.
+type CtxKey string
+
+// ContextKey is the key value required to access the cobra command context.
+const ContextKey CtxKey = "config"
+
+type (
+	// Config represents the options defined in the config file.
+	Config struct {
+		Directory string    `json:"directory"`
+		Editor    string    `json:"editor"`
+		Templates Templates `json:"templates"`
+	}
+)
+
+// Get fetches the config via viper and converts it to a config struct so it
+// can be used properly.
+func Get() (Config, error) {
+	conf := viper.AllSettings()
+
+	jsonContent, err := json.Marshal(conf)
+	if err != nil {
+		return Config{}, err
+	}
+
+	parsedConfig := Config{}
+	if err := json.Unmarshal(jsonContent, &parsedConfig); err != nil {
+		return Config{}, sentinel.Wrap(err, ErrUnmashallingJSON)
+	}
+
+	return parsedConfig, nil
+}
+
+// GetDirectory returns the directory value defined in config.
+func GetDirectory() (string, error) {
+	dir := viper.GetString("directory")
+	if dir == "" {
+		return "", errors.New("no directory defined in config file")
+	}
+
+	return dir, nil
+}
---
+++ internal/create/from_template_test.go
@@ -0,0 +1,185 @@
+package create_test
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tx3stn/pkb/internal/config"
+	"github.com/tx3stn/pkb/internal/create"
+)
+
+func TestGetFileName(t *testing.T) {
+	t.Parallel()
+
+	testTime, _ := time.Parse(time.RFC3339, "2022-09-19T16:20:00Z")
+
+	testCases := map[string]struct {
+		renderer    create.TemplateRenderer
+		expected    string
+		assertError require.ErrorAssertionFunc
+	}{
+		"uses prompt when no value in config": {
+			renderer: create.TemplateRenderer{
+				NamePrompt: func() (string, error) {
+					return "prompted for this string", nil
+				},
+				Templates: []config.Template{{}},
+			},
+			expected:    "prompted for this string",
+			assertError: require.NoError,
+		},
+		"combines values when mutiple provided": {
+			renderer: create.TemplateRenderer{
+				NamePrompt: func() (string, error) {
+					return "wow this is great", nil
+				},
+				SelectedTemplate: config.Template{
+					NameFormat: "{{.Date}}-{{.Prompt}}-{{.Week}}-{{.Year}}-foo",
+				},
+				Time: testTime,
+			},
+			expected:    "2022-09-19-wow this is great-38-2022-foo",
+			assertError: require.NoError,
+		},
+	}
+
+	for name, testCase := range testCases {
+		tc := testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			actual, err := tc.renderer.GetFileName()
+			tc.assertError(t, err)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestOutputPath(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		templateRenderer create.TemplateRenderer
+		assertError      require.ErrorAssertionFunc
+		expected         string
+	}{
+		"returns path for single template": {
+			templateRenderer: create.TemplateRenderer{
+				Config: config.Config{
+					Directory: "/home/username/notes",
+				},
+				Name: "simple.md",
+				Templates: []config.Template{
+					{
+						File:      "magic.tpl.md",
+						OutputDir: "magic",
+					},
+				},
+			},
+			assertError: require.NoError,
+			expected:    "/home/username/notes/magic/simple.md",
+		},
+		"creates full nested dir path when there are subtemplates": {
+			templateRenderer: create.TemplateRenderer{
+				Config: config.Config{
+					Directory: "/home/username/notes",
+				},
+				Name: "nested-example.md",
+				Templates: []config.Template{
+					{
+						File:      "foo.tpl.md",
+						OutputDir: "foo",
+					},
+					{
+						File:      "bar.tpl.md",
+						OutputDir: "bar",
+					},
+					{
+						File:      "wow.tpl.md",
+						OutputDir: "wow",
+					},
+				},
+			},
+			assertError: require.NoError,
+			expected:    "/home/username/notes/foo/bar/wow/nested-example.md",
+		},
+		"prompts user for directory input when specified in template config": {
+			templateRenderer: create.TemplateRenderer{
+				Config: config.Config{
+					Directory: "/home/username/notes",
+				},
+				Name: "simple.md",
+				DirectoryPrompt: func() (string, error) {
+					return "foo/dir", nil
+				},
+				Templates: []config.Template{
+					{
+						File:      "magic.tpl.md",
+						OutputDir: "{{.Prompt}}",
+					},
+				},
+			},
+			assertError: require.NoError,
+			expected:    "/home/username/notes/foo/dir/simple.md",
+		},
+	}
+
+	for name, testCase := range testCases {
+		tc := testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			actual, err := tc.templateRenderer.OutputPath()
+			tc.assertError(t, err)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestRender(t *testing.T) {
+	t.Parallel()
+
+	testTime, _ := time.Parse(time.RFC3339, "2022-09-19T16:20:00Z")
+
+	testCases := map[string]struct {
+		renderer        create.TemplateRenderer
+		templateContent string
+		expected        string
+		assertError     require.ErrorAssertionFunc
+	}{
+		"expands expected variables": {
+			renderer: create.TemplateRenderer{
+				Config: config.Config{
+					Templates: map[string]config.Template{},
+				},
+				Name: "example doc",
+				SelectedTemplate: config.Template{
+					CustomDateFormat: "Monday 2nd January",
+				},
+				Time: testTime,
+			},
+			templateContent: "{{.Date}}\n{{.Name}}\n{{.Time}}\n{{.CustomDateFormat}}\n{{.Week}}\n{{.Year}}",
+			expected:        "2022-09-19\nexample doc\n16:20\nMonday 19th September\n38\n2022",
+			assertError:     require.NoError,
+		},
+	}
+
+	for name, testCase := range testCases {
+		tc := testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			var actual bytes.Buffer
+			err := tc.renderer.Render(tc.templateContent, &actual)
+			tc.assertError(t, err)
+
+			assert.Equal(t, tc.expected, actual.String())
+		})
+	}
+}
