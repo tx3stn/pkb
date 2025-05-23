4362556faa01c862c57dbecd9f5702e3ebcef34c
tx3stn <14163530+tx3stn@users.noreply.github.com>
2025-05-23 15:24:40 +0100

    default edit behaviour to select existing and add specific open command

---
+++ cmd/open.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/tx3stn/pkb/internal/config"
+	"github.com/tx3stn/pkb/internal/editor"
+)
+
+// CreateOpen creates the new command "open" used to open your editor to edit existing notes.
+func CreateOpen() *cobra.Command {
+	cmd := &cobra.Command{
+		RunE: func(ccmd *cobra.Command, args []string) error {
+			conf, err := config.Get()
+			if err != nil {
+				return fmt.Errorf("error getting config: %w", err)
+			}
+
+			if err := editor.Open(conf.Editor, conf.Directory); err != nil {
+				return fmt.Errorf("error opening file in editor: %w", err)
+			}
+
+			return nil
+		},
+		Short: "open your notes directory in your specified editor",
+		Use:   "open",
+	}
+
+	return cmd
+}
